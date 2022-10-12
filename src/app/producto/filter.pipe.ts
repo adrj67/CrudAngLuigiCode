8da3cb435ff8b464fce9eb066f63be7cd7b89473
Adrian Rojo <adrj67@gmail.com>
2022-10-12 11:14:07 -0300

    Filtra listado total

--- src/app/producto/filter.pipe.ts
+++ src/app/producto/filter.pipe.ts
@@ -1,10 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
- @Pipe({
-    name: 'filter'
- })
+@Pipe({
+   name: 'filter'
+})
 
- export class FilterPipe implements PipeTransform { 
+export class FilterPipe implements PipeTransform { 
 
     transform(value: any, arg: any): any { 
         if(!arg || arg?.length < 3) return value; //comienza a buscar en la tercer letra escrita
@@ -14,4 +14,7 @@ import { Pipe, PipeTransform } from '@angular/core';
             if(producto.nombre.toLowerCase().indexOf(arg.toLowerCase()) > -1){ // toLowerCase:compara en minusculas
                 result.push(producto); 
             };
-         }; return result; } }
+        }; 
+        return result; 
+    }
+}
